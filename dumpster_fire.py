d604ebed222734e949dbef8462a58b4a39304052
Cayle Stickler <cayle.stickler@cosmicaes.com>
2018-02-16 16:52:34 -0700

    Adding code for the dumpster fire

--- dumpster_fire.py
+++ dumpster_fire.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 import traceback
 from time import sleep
+import sys
 
 class DumpsterFire:
     def __init__(self, dump_model):
@@ -10,7 +11,7 @@ class DumpsterFire:
         self.screen = curses.initscr()
         self.win_height = self.screen.getmaxyx()[0]
         self.win_width = self.screen.getmaxyx()[1]
-        self.dumpster_height = 15
+        self.dumpster_height = 10
         self.dumpster_width = 50
         curses.curs_set(0)
         curses.start_color()
@@ -29,30 +30,65 @@ class DumpsterFire:
         width_end = self.win_width // 2 + self.dumpster_width // 2
         dumpster_top = self.win_height - self.dumpster_height
         size = self.dumpster_width * self.dumpster_height
-        buffer = np.zeros((self.dumpster_height, self.dumpster_width - 2), dtype=np.int)
+        buffer = np.zeros(
+            (self.dumpster_height,
+             self.dumpster_width),
+            dtype=np.int)
 
         while True:
             for i in range(width_start, width_end):
                 self.screen.addstr(
                     self.win_height - 1, i, "-", curses.color_pair(5))
                 self.screen.addstr(dumpster_top, i, "-", curses.color_pair(5))
+                self.screen.addstr(dumpster_top + self.dumpster_height // 2 - 2, i, "-", curses.color_pair(5))
+                self.screen.addstr(dumpster_top + self.dumpster_height // 2 + 2, i, "-", curses.color_pair(5))
+                self.screen.addstr(dumpster_top - 3, i + 3, "-", curses.color_pair(5))
 
             for i in range(self.win_height - self.dumpster_height,
                            self.win_height):
                 self.screen.addstr(i, width_start, "|", curses.color_pair(5))
                 self.screen.addstr(i, width_end, "|", curses.color_pair(5))
 
+            for i in range(self.win_height - self.dumpster_height,
+                           self.win_height + 1):
+                self.screen.addstr(i - 3, width_end + 3, "|", curses.color_pair(5))
+
+
+
+            for i in range(1,3):
+                self.screen.addstr(
+                        dumpster_top - i, 
+                        width_end + i,
+                        "/",
+                        curses.color_pair(5))
+                self.screen.addstr(
+                        dumpster_top + self.dumpster_height - i,
+                        width_end + i,
+                        "/",
+                        curses.color_pair(5))
+
+
+            model_start_x = width_start + \
+                (self.dumpster_width // 2) - len(self.dump_model) // 2
+            model_start_y = dumpster_top + (self.dumpster_height // 2)
+            self.screen.addstr(
+                model_start_y,
+                model_start_x,
+                self.dump_model,
+                curses.color_pair(5))
+
             # Only attempt to start 7 fires.
             for i in range(7):
-                buffer[0, int(random.random() * (self.dumpster_width - 3))] = 65
+                buffer[0,
+                       int(random.random() * (self.dumpster_width - 3))] = 65
 
             for i in range(self.dumpster_height):
                 for j in range(self.dumpster_width - 3):
                     buffer[i, j] = (buffer[i, j] +
-                                    buffer[i, j + 1] + 
+                                    buffer[i, j + 1] +
                                     buffer[i - 1, j] +
                                     buffer[i - 1, j + 1]) // 4
-                
+
                     color = 1
                     if buffer[i, j] > 15:
                         color = 4
@@ -63,7 +99,8 @@ class DumpsterFire:
 
                     self.screen.addstr(dumpster_top - i - 1,
                                        width_start + 2 + j,
-                                       chars[9 if buffer[i,j] > 9 else buffer[i, j]],
+                                       chars[9 if buffer[i, j] >
+                                             9 else buffer[i, j]],
                                        curses.color_pair(color) | curses.A_BOLD)
 
             sleep(0.016)
@@ -76,9 +113,12 @@ class DumpsterFire:
 
 
 if __name__ == "__main__":
-    dump = DumpsterFire("Grizzly")
+    name = "My Life"
+    if len(sys.argv) >= 2:
+        name = sys.argv[1]
+    dump = DumpsterFire(name)
     try:
         dump.render()
-    except:
+    except BaseException:
         curses.endwin()
         print(traceback.format_exc())
